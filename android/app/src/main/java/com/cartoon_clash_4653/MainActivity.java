dc7d79d95b36f16a3dd743546a801f0ffec46eac
Crowdbotics <team@crowdbotics.com>
2019-06-14 04:26:24 +0000

    initial commit

---
+++ android/app/src/main/java/com/cartoon_clash_4653/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cartoon_clash_4653;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "cartoon_clash_4653";
+    }
+}
